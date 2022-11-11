5985b5b5992a08e6f99db6c92d76b4747edf2ef8
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2022-11-11 16:34:58 +0100

    Add database support

---
+++ client/src/About.js
@@ -0,0 +1,83 @@
+import React from "react"
+
+export default function About() {
+    return (
+        <div className="max-w-[40%] min-w-[10%] m-auto leading-7">
+            <h1 className="text-4xl font-bold mb-2 pb-2">About Us</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec
+                ullamcorper leo. Mauris diam risus, faucibus eu fringilla sed, aliquam
+                eget risus. Donec blandit, purus quis ornare accumsan, nisl sem faucibus
+                ante, id elementum nisi erat aliquam purus. Donec ullamcorper massa a
+                mauris maximus interdum. In gravida ex eu est euismod varius. Nulla
+                placerat tincidunt suscipit. Aliquam sollicitudin, lorem vel varius
+                mattis, libero urna laoreet nibh, nec rhoncus libero lacus non nibh
+                Proin elit risus, volutpat ac maximus a, feugiat vel turpis. Phasellus
+                laoreet, magna at ullamcorper varius, erat nibh condimentum odio, et
+                lacinia neque nulla at mi. In malesuada vestibulum lacinia. Curabitur
+                porta suscipit ex sit amet efficitur. Cras tempus convallis ullamcorper.
+                Nulla a quam ac nunc ornare laoreet. Morbi a turpis viverra, ultrices
+                odio id, dictum ex. Donec arcu sem, bibendum id felis sit amet,
+                tincidunt bibendum orci. Phasellus ac elit elementum, euismod felis in,
+                consequat est.{" "}
+            </p>
+            <p>
+                {" "}
+                Praesent tellus metus, interdum quis arcu sed, consequat rutrum dolor.
+                Suspendisse eu felis vel augue lacinia pretium ut vitae nisl. Sed
+                euismod enim ac nunc dapibus tempor. Nulla magna turpis, tincidunt sit
+                amet ultricies et, dignissim vel nisi. Aliquam a tempor ex. Donec tempor
+                tristique risus, vel lacinia enim sagittis eget. Maecenas sit amet risus
+                quis lacus vestibulum volutpat. Sed at mauris sit amet neque dignissim
+                rutrum. Ut vel odio non erat mollis molestie vehicula id sapien. Etiam
+                vel lorem vel arcu faucibus rhoncus. Cras varius magna in lorem
+                porttitor, nec gravida orci rhoncus. Cras facilisis malesuada magna.
+                Maecenas accumsan augue sit amet magna dapibus hendrerit. Nullam lorem
+                nulla, sagittis vitae pulvinar vel, rutrum vel odio. Sed in dignissim
+                odio, at imperdiet nulla.
+            </p>
+            <p>
+                hasellus commodo hendrerit accumsan. Donec euismod vel ex ac pharetra.
+                Donec a arcu tincidunt, blandit arcu efficitur, dignissim enim.
+                Suspendisse id suscipit ex, vitae sodales velit. Cras iaculis justo eget
+                nibh malesuada fringilla. Morbi metus libero, viverra a placerat eu,
+                rhoncus sed massa. Integer eget erat scelerisque, porttitor lorem eget,
+                pretium lorem. Nulla a interdum tellus. Integer ultricies bibendum
+                massa, sed ultrices tortor congue sed. Quisque feugiat luctus tempor.
+                Phasellus leo mi, elementum in consectetur sed, tincidunt vitae lorem.
+                Praesent tortor ex, laoreet ut dolor in, congue ultrices urna. Vivamus
+                magna magna, semper nec diam id, rhoncus varius lacus. Suspendisse
+                aliquet urna ac tortor rhoncus consectetur. Pellentesque a tortor vel
+                enim maximus auctor ac eu erat. Praesent ultricies viverra aliquet.{" "}
+            </p>
+            <p>
+                {" "}
+                Cras non lorem auctor, aliquet nulla eget, ultrices ipsum. Curabitur ut
+                sodales quam. Nulla maximus porta tellus, in maximus ligula vulputate
+                at. Aenean lacinia magna vitae elit dapibus tempus a a nisi. Proin
+                feugiat convallis lectus. Quisque hendrerit purus eget magna eleifend
+                lobortis. Nullam consequat neque sagittis leo feugiat lacinia. Morbi
+                metus sapien, pretium nec tincidunt non, tincidunt at sapien. Mauris
+                accumsan felis sapien. In cursus nibh mi, at sagittis lorem pulvinar in.
+                Curabitur eget felis semper, cursus lorem quis, finibus massa. Proin
+                dictum nisi nec leo sollicitudin, finibus venenatis dui ultricies.
+                Aenean quis bibendum elit.{" "}
+            </p>
+            <p>
+                as odio nibh, bibendum a consequat sed, convallis nec eros. Praesent ex
+                nibh, blandit malesuada massa vel, ultricies pulvinar orci. Vestibulum
+                mi nibh, posuere vitae viverra eu, porta varius orci. Curabitur cursus
+                mi sapien, sed venenatis nibh elementum sit amet. Morbi mollis enim quis
+                odio suscipit molestie. Vestibulum malesuada ipsum ac orci blandit
+                imperdiet. Maecenas eu sapien velit. Fusce gravida, purus eget porta
+                imperdiet, nisl elit malesuada sem, vel condimentum ipsum nisi sed
+                sapien. Nullam gravida urna quis vehicula ornare. Mauris dolor risus,
+                vulputate id volutpat a, ultrices quis sem. Interdum et malesuada fames
+                ac ante ipsum primis in faucibus. Ut efficitur auctor ex euismod auctor.
+                Integer sodales mauris odio, sit amet efficitur enim finibus tempus. Sed
+                metus urna, accumsan nec tempus vel, eleifend in ligula. Aenean lacinia
+                magna in lacinia porta. Ut quis lectus a diam consequat eleifend.{" "}
+            </p>
+        </div>
+    )
+}
--- client/src/App.js
+++ client/src/App.js
@@ -2,27 +2,20 @@ import "./App.css"
 // import Front from "./Front"
 import Navbar from "./Navbar"
 import ItemCard from "./ItemCard"
+// import About from "./About"
 
 function App() {
+  const database = JSON.parse(
+    JSON.stringify(require("./data/database.json"))
+  ).products
+  const cards = database.map((product) => (
+    <ItemCard image={product.image} name={product.name} price={product.price} />
+  ))
   return (
     <div>
       <Navbar />
-      <div className="flex justify-center gap-x-10">
-        <ItemCard
-          image="https://www.wmfnordic.com/assets/image-cache/images/media/3D7B8EA2-1EAF-4693-BB14B2EA0E14FE42.a83d4af7.jpg"
-          price="199.95"
-          name="Pan"
-        />
-        <ItemCard
-          image="https://www.ikea.com/us/en/images/products/ikea-365-pot-with-lid-stainless-steel__1006171_pe825756_s5.jpg"
-          price="299.95"
-          name="Pot"
-        />
-        <ItemCard
-          image="https://media.wired.com/photos/5fcaa4cbd840498ab676cf84/master/w_2580,c_limit/Gear-Lodge-Combo-Cooker-SOURCE-Lodge.jpg"
-          price="399.95"
-          name="Pan & Pot Combo jalepenos"
-        />
+      <div className="max-w-10 mx-20 my-10 grid gap-x-10 gap-y-20 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+        {cards}
       </div>
     </div>
   )
