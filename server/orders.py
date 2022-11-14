7f7f03f287fe05e279d5e51fcfb95bea550ee117
William Bundgaard <william.bundgd@gmail.com>
2022-11-14 19:56:08 +0100

    made a lot of API specifications, and sat up some more logic for the server code

--- server/orders.py
+++ server/orders.py
@@ -2,7 +2,31 @@ class Order(object):
 
     allOrders = []
 
-    def __init__(self, orderId):
-        self.id = orderId
+    def __init__(self, data):
+        self.id = data["ID"]]
+        self.data = data
+        self.open = True
 
         allOrders.add(self)
+
+    def getAllOrders():
+        return allOrders
+
+    def getAllOpenOrders():
+        openOrders = []
+        for order in allOrders:
+            if order.open:
+                openOrders.add(order)
+        return openOrders
+
+    def export():
+        return self.data
+
+    def setState(newState):
+        oldState = self.open
+        self.open = newState
+        return oldState = newState
+
+    def cancle():
+        allOrders.remove(self)
+        
