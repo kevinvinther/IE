511dc38f4786f32f448a1f7b78e06a47109f5a8e
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2022-11-23 11:40:25 +0100

    Add code to make basket inc/decrementer work. Add alt text to image.

--- client/src/ItemCard.js
+++ client/src/ItemCard.js
@@ -1,9 +1,47 @@
 import React from "react"
 
 export default function ItemCard({ image, price, name }) {
+    // The code before the return is from https://tailwindcomponents.com/component/number-input-counter
+    // This is the code that controls the counter.
+    // The counter as well is from the before mentioned site.
+    function decrement(e) {
+        const btn = e.target.parentNode.parentElement.querySelector(
+            'button[data-action="decrement"]'
+        )
+        const target = btn.nextElementSibling
+        let value = Number(target.value)
+        value--
+        target.value = value
+    }
+
+    function increment(e) {
+        const btn = e.target.parentNode.parentElement.querySelector(
+            'button[data-action="decrement"]'
+        )
+        const target = btn.nextElementSibling
+        let value = Number(target.value)
+        value++
+        target.value = value
+    }
+
+    const decrementButtons = document.querySelectorAll(
+        `button[data-action="decrement"]`
+    )
+
+    const incrementButtons = document.querySelectorAll(
+        `button[data-action="increment"]`
+    )
+
+    decrementButtons.forEach((btn) => {
+        btn.addEventListener("click", decrement)
+    })
+
+    incrementButtons.forEach((btn) => {
+        btn.addEventListener("click", increment)
+    })
     return (
         <div className="block p-6 max-w-sm min-w-[300px] max-h-900 min-h-[250px] bg-white rounded-lg border border-gray-200 shadow-md hober:bg-gray-100 ">
-            <img src={image} className="object-contain h-80 w-90" />
+            <img src={image} alt={name} className="object-contain h-80 w-90" />
             <div className="grid grid-cols-2 justify-between">
                 <h5 class="mb-0 text-2xl font-bold tracking-tight text-gray-900 dark:text-white truncate">
                     {name}
