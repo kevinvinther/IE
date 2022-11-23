8b680ee5377796eeccecfb1a864325c59f40fb42
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2022-11-23 15:12:59 +0100

    Add total sum in shopping cart

--- client/src/ShoppingCart.js
+++ client/src/ShoppingCart.js
@@ -1,5 +1,7 @@
 import { useShoppingCart } from "./context/ShoppingCartContext"
 import { CartItem } from "./CartItem.js"
+import { cart } from "./context/ShoppingCartContext.js"
+import storeItems from "./data/database.json"
 
 export default function ShoppingCart({ isOpen }) {
     const { closeCart, cart } = useShoppingCart()
@@ -20,6 +22,16 @@ export default function ShoppingCart({ isOpen }) {
                         {cart.map((item) => (
                             <CartItem key={item.sku} {...item} />
                         ))}
+                    </div>
+                    <div className="text-2xl font-bold text-right">
+                        Total{" "}
+                        {cart.reduce((total, cartItem) => {
+                            const item = storeItems.products.find(
+                                (item) => item.sku === cartItem.sku
+                            )
+                            return total + (item?.price || 0) * cartItem.quantity
+                        }, 0)}
+                        dkk
                     </div>
                     <button className="text-white rounded shadow p-3 bg-blue-500 mt-10">
                         Goto checkout
