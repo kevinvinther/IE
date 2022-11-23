4a68d25a05178152dc6d6b3b8f9b65c3ed8f2195
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2022-11-23 12:06:04 +0100

    Remove input field, and change it to a span.
    Also removes the logic, as it did not work properly. Instead opting to
    make react code.

--- client/src/ItemCard.js
+++ client/src/ItemCard.js
@@ -1,44 +1,7 @@
 import React from "react"
 
 export default function ItemCard({ image, price, name }) {
-    // The code before the return is from https://tailwindcomponents.com/component/number-input-counter
-    // This is the code that controls the counter.
-    // The counter as well is from the before mentioned site.
-    function decrement(e) {
-        const btn = e.target.parentNode.parentElement.querySelector(
-            'button[data-action="decrement"]'
-        )
-        const target = btn.nextElementSibling
-        let value = Number(target.value)
-        value--
-        target.value = value
-    }
-
-    function increment(e) {
-        const btn = e.target.parentNode.parentElement.querySelector(
-            'button[data-action="decrement"]'
-        )
-        const target = btn.nextElementSibling
-        let value = Number(target.value)
-        value++
-        target.value = value
-    }
-
-    const decrementButtons = document.querySelectorAll(
-        `button[data-action="decrement"]`
-    )
-
-    const incrementButtons = document.querySelectorAll(
-        `button[data-action="increment"]`
-    )
-
-    decrementButtons.forEach((btn) => {
-        btn.addEventListener("click", decrement)
-    })
-
-    incrementButtons.forEach((btn) => {
-        btn.addEventListener("click", increment)
-    })
+    let quantity = 1
     return (
         <div className="block p-6 max-w-sm min-w-[300px] max-h-900 min-h-[250px] bg-white rounded-lg border border-gray-200 shadow-md hober:bg-gray-100 ">
             <img src={image} alt={name} className="object-contain h-80 w-90" />
@@ -46,24 +9,18 @@ export default function ItemCard({ image, price, name }) {
                 <h5 class="mb-0 text-2xl font-bold tracking-tight text-gray-900 dark:text-white truncate">
                     {name}
                 </h5>
-                <div class="custom-number-input h-10 w-32 justify-self-end">
+                <div class="h-10 w-32 justify-self-end">
                     <div class="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
-                        <button
-                            data-action="decrement"
-                            class=" bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none"
-                        >
+                        <button class=" bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none">
                             <span class="m-auto text-2xl font-thin">−</span>
                         </button>
-                        <input
+                        <span
                             type="number"
-                            class="focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700  outline-none"
-                            name="custom-input-number"
-                            value="0"
-                        ></input>
-                        <button
-                            data-action="increment"
-                            class="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer"
+                            class="justify-center focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700  outline-none"
                         >
+                            {quantity}
+                        </span>
+                        <button class="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer">
                             <span class="m-auto text-2xl font-thin">+</span>
                         </button>
                     </div>
